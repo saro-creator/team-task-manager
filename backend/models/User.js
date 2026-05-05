f178e6afc68d0aaf5a45654dd65d89877f34971d
Alok Saroj <150350125+Xlokyaroj@users.noreply.github.com>
2026-05-05 13:24:06 +0530

    new uppdet

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,11 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['Admin', 'Member'], default: 'Member' }
+  //role: { type: String, enum: ['Admin', 'Member'], default: 'Member' }
+  // models/User.js mein
+role: { type: String, enum: ['user', 'admin'], default: 'user'}
 });
+
+//
 
 module.exports = mongoose.model('User', UserSchema);
